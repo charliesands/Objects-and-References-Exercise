29282246fc1e6ba046f4d621cd285db47a1320a3
charliesands <cwsands3@gmail.com>
2018-11-13 17:15:23 -0700

    elephant buttons working

--- ObjectAndReferencesExercise/Form1.cs
+++ ObjectAndReferencesExercise/Form1.cs
@@ -12,9 +12,29 @@ namespace ObjectAndReferencesExercise
 {
     public partial class Form1 : Form
     {
+        Elephant lucinda;
+        Elephant lloyd;
+
         public Form1()
         {
             InitializeComponent();
+            lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
